8a291434b614381a910994b6a8722b69e0f86710
kasra-l83 <o.kasralatifi@gmail.com>
2024-11-21 22:57:46 +0330

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header(){
+  return(
+    <div className="flex flex-col items-center h-28 justify-between pt-2">
+      <span className="flex items-center">
+        <img src="img/logo.png" className="size-16 mr-2"/>
+        <h1 className="text-4xl font-black">Starbucks</h1>
+      </span>
+      <h4 className="text-lg font-bold text-darkGreen">Starbucks Online Coffee Order</h4>
+    </div>
+  )
+}
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    colors: {
+      darkGreen: "#1E3932"
+    },
+    extend: {}
+  },
+  plugins: []
+}
